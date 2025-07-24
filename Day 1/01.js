62724401ba91defc6df05bbb9ea09c360202f2dd
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-07-24 18:37:35 +0530

    day-1: Learn't basic var, let and const and data types

---
+++ Day 1/01.js
@@ -0,0 +1,13 @@
+// var a;
+// let b = 20;
+// b = "abc"
+
+// {
+//     var a = 20;
+//     console.log(a);
+//     let b = 30;
+//     console.log(b);
+// }
+
+// console.log(a)
+// console.log(b)
---
+++ Day 1/01.ts
@@ -0,0 +1,26 @@
+// // Primitives: number string boolean
+// // Non : Array Object any undefined null 
+
+// type listType = number | string;
+
+// interface list1Interface {
+//     0: string
+//     1: number
+//     name: string
+// }
+
+// let aNumber: number;
+// let bnumber = 20;
+
+// const list1: list1Interface = {
+//     0: "snjnc",
+//     1: 1983,
+//     name: "shubhojit",
+// }
+
+// let list: string[] = ["snjnc", "1983"]
+
+
+// console.log(list1["name"]);
+
+
